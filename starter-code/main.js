1d5df494025f42225f43fa57a3311ceb8e71129a
ElliotMichael <erhunmwuoseree@gmail.com>
2023-09-05 00:54:58 -0400

    Aligned and fixed the arrangements of the header icons

--- starter-code/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.querySelector(".toggle-button");
-
-    toggleButton.addEventListener("click", function () {
-        toggleButton.classList.toggle("active");
-        // You can add additional logic here to switch other elements to dark mode
-    });
-});
